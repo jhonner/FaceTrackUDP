9e049c647e49b2ea051f36cb83815952e6bdd0bc
Yann Bouschet <yann.bouschet@sap.com>
2022-05-11 13:22:32 -0400

    Initial Commit

---
+++ FaceTrackUDP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceTrackUDP
+//
+//  Created by Yann Bouschet on 2022-05-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
